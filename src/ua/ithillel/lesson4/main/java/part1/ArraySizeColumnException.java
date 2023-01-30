a3ac4ea1bdf74b5f6804effe65e9a2551de7cccb
kateryna_rozhkovska@epam.com <kateryna_rozhkovska@epam.com>
2023-01-31 01:12:41 +0200

    Added homework4

---
+++ src/ua/ithillel/lesson4/main/java/part1/ArraySizeColumnException.java
@@ -0,0 +1,11 @@
+package part1;
+
+public class ArraySizeColumnException extends IndexOutOfBoundsException {
+    public ArraySizeColumnException() {
+    }
+
+    public ArraySizeColumnException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/ua/ithillel/lesson4/main/java/part1/MainClass.java
@@ -0,0 +1,23 @@
+package part1;
+
+import java.util.Random;
+
+import static part1.ArrayValueCalculator.doCalc;
+import static part1.ArrayValueFiller.doFill;
+
+public class MainClass {
+    public final static int MAX_ARRAY_LENGTH = 4;
+
+    // if you want to get ArraySizeColumnException or ArraySizeRowException you should change array's size manually
+    static String[][] inputArr = new String[4][5];
+    private static final int FAULT_OR_NOT = new Random().nextInt(2);
+
+    public static void main(String[] args) {
+        try {
+            doFill(inputArr, FAULT_OR_NOT);
+            System.out.println(String.format("Sum of array's cells is %s", doCalc(inputArr)));
+        } catch (ArraySizeColumnException | ArrayDataExeption | ArraySizeRowException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
